345f58d7082e97a13e96888852d9b461a3e0d10f
morfeo2013 <jhonrios@hotmail.es>
2021-04-03 21:29:51 -0500

    incluye inicio2

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 
 
@@ -6,9 +8,26 @@ import React from 'react'
 export default function inicio() {
     return (
         <div className="container">
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <div className="row">
 
-                <div className="col-7 mt-5">
+                <div className="col-6 mt-5">
                     <h3> "Salud y Bienestar al alcance de tus manos"</h3>
                     <h4 className="mt-5">Enlaces</h4>
                     <ul className="mt-5"
@@ -37,12 +56,43 @@ export default function inicio() {
                     </ul>
                 </div>
 
+                <div className="col-1 mt-5">
+                 
 
-                <div className="col-5 mt-4" id="imagen-animada">
-                    <img id="imagen-animada" src=" https://res.cloudinary.com/dhiasghho/image/upload/v1617417985/logo_central2_dews6n.jpg" alt=""
-                        className=" img-fluid"   ></img>
+                </div >
+                {(screen.width >700)?
+                   
+                    <div className="col-3 mt-4 " >
+                    <div className='content-all'>
+                    <div className='content-carrusel'>
+                        
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img1_lpscvh.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img2_b553ro.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img3_a7djje.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img4_ffr0gi.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img5_tsw14o.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img6_nvhsyr.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img7_egtwkm.png" /></figure>
+
+                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img8_ximxmh.png" /></figure>
+
+                  </div>
+                    </div>
+                 
 
                 </div>
+
+                :
+                 null
+
+                }
+
             </div>
 
 
