1f150c9aa1f74baaf2a3396275ecfdcf53e4ac76
morfeo2013 <jhonrios@hotmail.es>
2021-04-04 19:21:30 -0500

    primer vercion

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -9,7 +9,7 @@ export default function inicio() {
     return (
         <div className="container">
 
-         
+
 
 
 
@@ -33,65 +33,63 @@ export default function inicio() {
                     <ul className="mt-5"
                     >
                         <li>
-                        Que es nuestra marca personal
+                            Que es nuestra marca personal
 
                     </li>
 
                         <li>
-                        Productos
+                            Productos
 
                     </li>
                         <li>
-                        Testimonios
+                            Testimonios
 
                     </li>
-                    <li>
-                    PQRS
+                        <li>
+                            PQRS
 
                     </li>
-                    <li>
-                    Políticas de privacidad 
+                        <li>
+                            Políticas de privacidad
 
                     </li>
                     </ul>
                 </div>
 
                 <div className="col-1 mt-5">
-                 
+
 
                 </div >
-                {(screen.width >700)?
-                   
+             
+
                     <div className="col-3 mt-4 " >
-                    <div className='content-all'>
-                    <div className='content-carrusel'>
-                        
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img1_lpscvh.png" /></figure>
+                        <div className='content-all'>
+                            <div className='content-carrusel'>
+                                <figure><img src="https://morfeo12345678.s3-sa-east-1.amazonaws.com/imgLogo.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img2_b553ro.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img1_lpscvh.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img3_a7djje.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img2_b553ro.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img4_ffr0gi.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img3_a7djje.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img5_tsw14o.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img4_ffr0gi.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img6_nvhsyr.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img5_tsw14o.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img7_egtwkm.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489816/img6_nvhsyr.png" /></figure>
 
-                        <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img8_ximxmh.png" /></figure>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img7_egtwkm.png" /></figure>
 
-                  </div>
+                                <figure><img src="https://res.cloudinary.com/dhiasghho/image/upload/v1617489817/img8_ximxmh.png" /></figure>
+
+                            </div>
+                        </div>
+
+
                     </div>
-                 
 
-                </div>
-
-                :
-                 null
-
-                }
+                   
 
             </div>
 
