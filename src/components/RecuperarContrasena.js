9f09382e16cf4dff49c418369be3a910064b34f2
morfeo2013 <jhonrios@hotmail.es>
2021-04-11 22:42:19 -0500

    recuperacion envio fronent1

---
+++ src/components/RecuperarContrasena.js
@@ -0,0 +1,86 @@
+import Axios from 'axios'
+import React, { useState } from 'react'
+import Swal from 'sweetalert2'
+import { Link } from 'react-router-dom'  /* IMPORTAR PARA PODER ACCEDER AL PA PROPIEDAD LINK Y ACCEDER
+A LA PAGINA DONDE ESTA CREADO LA OPCION DE  CREAR USUARIOS EN EL RETUR DE CREACION DE PAGINAS */
+
+
+export default function RecuperarContrasena() {
+    const [correo, setCorreo] = useState('')
+
+    const login = async (e) => {
+
+        e.preventDefault()
+
+        /* este recivira los datos desda la pagina para ser comparados con los del backend */
+        const RecuperarContrasena = {
+            correo
+        }
+        const respuesta = await Axios.post('http://localhost:4000/Recuperar', RecuperarContrasena)/* 
+        enviara los datos json de la onstante usuario al backend */
+
+        const mensaje = respuesta.data.mensage /* extraemos el mensaje desde el backen creado para mostrarlo en el sweetalert2 */
+
+        if (mensaje === 'Tu Contraseña a sido enviada a tu Correo desde el backend') {
+            Swal.fire({
+                icon: 'success',
+                title: mensaje,
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: mensaje,
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+
+        }
+
+
+    }
+
+    return (
+        <div>
+            <div className="row pt-4">
+            <div className="col-lg-6 col-md-8 mx-auto">
+                <div className="card">
+                    <div className="container text-center fa-4x">
+                        <i className="fas fa-user"></i>
+                    </div>
+                    <div className="card-header text-center">
+                        Recuperar Contraseña
+
+                   </div>
+
+                    <div className="card-boddy m-2">
+                        <form onSubmit={login}>
+                            <div className="form-group mt-2">
+                                <input type="email" name="correo" className="form-control"
+                                    /* autofocus inicia el cursor en este lugar del form */
+                                    placeholder="Ingresa tu Correo" required autoFocus onChange={e => setCorreo(e.target.value)}
+                                    value={correo} />
+
+                            </div>
+                            
+                            <div className="d-grid gap-2 m-2">
+
+                                <button type="submit" className=" btn btn-primary mt-3 btn-block">Aceptar</button>
+                            </div>
+                        </form>
+
+                    </div>
+                    
+                   
+
+                </div>
+               
+            </div>
+
+        </div>  
+        </div>
+    )
+}
