0c8af2989e97011d7be1ab40ec0bc7a4a448c3b3
unknown <jhonrios@hotmail.es>
2021-06-01 00:09:10 -0500

    modificaciones envio mensages recuperacion contraseña 6

--- src/views/cambiarpassword.js
+++ src/views/cambiarpassword.js
@@ -2,91 +2,74 @@
 import Axios from "axios";
 import React, { useState } from "react";
 
-
 /* exportarlo como componente para inyectar en public.routes.js*/
-export default function  cambiarpassword (props){
-   var [infoPassword, setInfoPassword] = useState('');   
-/* se toman las PROPS para sacar el valor del la URL */
+export default function cambiarpassword(props) {
+  var [newPassword, setNewPassword] = useState("");
+  /* se toman las PROPS para sacar el valor del la URL */
 
- /* tomamos el valor que envia el baken por la url y la destructuramos */
-   const  { match: { params } } = props; 
+  /* tomamos el valor que envia el baken por la url y la destructuramos */
+  const {
+    match: { params },
+  } = props;
 
-/* props.match.path === "/comprar/:id" EL .PACH ES PARA IDENTIFICAR SI TIENE DETERMINADA INFORMACION LA URL */
-/*  props.match.params.id  EL .PARMAS ES PARA SACAR LA INFORMACION DE ESA HUBICACION EN LA URL ATRAVEX DE LOS PROPS QUE LLEGAN DESDE PUBLIC.ROUTE*/ 
+  /* props.match.path === "/comprar/:id" EL .PACH ES PARA IDENTIFICAR SI TIENE DETERMINADA INFORMACION LA URL */
+  /*  props.match.params.id  EL .PARMAS ES PARA SACAR LA INFORMACION DE ESA HUBICACION EN LA URL ATRAVEX DE LOS PROPS QUE LLEGAN DESDE PUBLIC.ROUTE*/
 
-   /* se toma el valor establecido en  el archivo de public.route
+  /* se toma el valor establecido en  el archivo de public.route
     <PublicRoute exact path = "/cambiopassword/:id1/:id2" component={cambiarpassword} */
 
-    /* DOS MANERAS ES LO MISMO */
-   const tokenTiempo =(props.match.params.id1)/* SE TOMA DERECTO DE LOS PROPS */
-   const idUsuario =(params.id2) /* SE DESTRUCTURA CON   const  { match: { params } } = props; */
-   const newPassword='fredy'
-  console.log(tokenTiempo) // mostraria el token
-  console.log(setInfoPassword) // mostraria elid del usuario
+  /* DOS MANERAS ES LO MISMO */
+  const tokenTiempo = props.match.params.id1; /* SE TOMA DERECTO DE LOS PROPS */
+  const idUsuario =
+    params.id2; /* SE DESTRUCTURA CON   const  { match: { params } } = props; */
 
-/* ENVIOS DE LA INFORMACIOIN AL BACKEND PARA LA MODIFICACION DE LA CONTRASEÑA */
+  console.log(tokenTiempo); // mostraria el token
 
+  /* ENVIOS DE LA INFORMACIOIN AL BACKEND PARA LA MODIFICACION DE LA CONTRASEÑA */
 
- setInfoPassword= {idUsuario,newPassword}
- console.log(setInfoPassword) // mostraria elid del usuario
-
-
-
-
-const envioNuevoPasswors =async ()=>{
-
-
-  
-   const respuesta = await Axios.post(
-     /*  "https://ganohealthy.herokuapp.com/cambio_password/",
+  const envioNuevoPasswors = async () => {
+    const respuesta = await Axios.get(
+      /*  "https://ganohealthy.herokuapp.com/cambio_password/",
       tokenTiempo,infoPassword */
 
-      "http://localhost:4000/cambio_password/"+
-     idUsuario +
-     "/" +
-     newPassword,{ headers: {'autorizacion':'bearer ' +tokenTiempo} }
-     , 
+      'http://localhost:4000/cambio_password/' + idUsuario + '/' + newPassword , { headers: { autorizacion: "bearer " + tokenTiempo } }
+     
     );
- 
-  console.log(respuesta)
-  console.log(infoPassword)
 
-    
+    const mensaje =
+      respuesta.data
+        .mensage;
+        
+    console.log(respuesta.data
+        .mensage);
+  };
 
-
-
-
-
-    
-   } 
-    
-   return(
-      <div>
+  return (
+    <div>
       <p>Pagina para cambiar contraseña</p>
-      <button
-                    className="btn btn-danger mr-2"
-                    id="desactivar"
-                    onClick={() => envioNuevoPasswors(infoPassword)}
-                  >
-                    <i className="far fa-trash-alt"> </i>{" "}
-                    {/* agregar el onClick para ejecutaar la funcion eliminar APROVECHA Y SACA EL ._ID Y LO ENVIA A LA FUNCION ELIMINAR(_ID)*/}
-                    Eliminar
-                  </button>
-    
+
+      <div className="form-group  mt-3">
+        <input
+          type="text"
+          className="form-control"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value.toLowerCase())}
+          placeholder="codigo"
+          required
+        />
       </div>
-      )
-    
-    
-
-
+      <button
+        className="btn btn-danger mr-2"
+        id="desactivar"
+        onClick={() => envioNuevoPasswors()}
+      >
+        <i className="far fa-trash-alt"> </i>{" "}
+        {/* agregar el onClick para ejecutaar la funcion eliminar APROVECHA Y SACA EL ._ID Y LO ENVIA A LA FUNCION ELIMINAR(_ID)*/}
+        Eliminar
+      </button>
+    </div>
+  );
 }
-
-
-
-
-
- 
-
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* otra manera de hacerlo */
@@ -97,7 +80,6 @@ const envioNuevoPasswors =async ()=>{
    </div>
    )
 } */
-
 
 /* export default cambiarpassword;  */
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
