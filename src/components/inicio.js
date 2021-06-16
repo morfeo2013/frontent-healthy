5bcb04c810d0fa0f04d7625629eb3d0dec8d25b8
unknown <jhonrios@hotmail.es>
2021-06-15 20:47:02 -0500

    mejoras graficas posicion boton navbar3

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -2,23 +2,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-undef */
 
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 /*CREAMOS UN ESTADO PARA CONTROLAR NO MOSTRAR EL MODAL CUANDO ESTE LOGIADO   */
-
-
 
 $(document).ready(function () {
   $("#staticBackdrop").modal("show");
-  
 });
 
-
-
-
 export default function inicio() {
-
   const [menu, setMenu] = useState(false);
-
 
   useEffect(() => {
     if (sessionStorage.getItem("token")) {
@@ -26,121 +18,121 @@ export default function inicio() {
     }
   }, [menu]);
 
-
   return (
     <div className="" id="inicio" autofocus>
       {/* CREACION DEL MODAL AL INICIO DE AL PAGINA */}
-      {menu ? null
-      :
-      
-      <div
-        class="modal fade"
-        id="staticBackdrop"
-        tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-          <div class="modal-content" id="llamado-accion">
-            <div class="modal-header">
-              <div>
-                <div className="row text-center">
-                  <button
-                    type="button"
-                    class="btn-close text-top"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
-                  <h4 class="modal-title" id="staticBackdropLabel">
-                    {" "}
-                    ¡Bienvenido a{" "}
-                    <font color="Green">GANO HEALTHY MEDELLIN!</font>
-                  </h4>
+      {
+        menu ? null : (
+          <div
+            class="modal fade"
+            id="staticBackdrop"
+            tabindex="-1"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+              <div class="modal-content" id="llamado-accion">
+                <div class="modal-header">
+                  <div>
+                    <div className="row text-center">
+                      <button
+                        type="button"
+                        class="btn-close text-top"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                      <h4 class="modal-title" id="staticBackdropLabel">
+                        {" "}
+                        ¡Bienvenido a{" "}
+                        <font color="Green">GANO HEALTHY MEDELLIN!</font>
+                      </h4>
+                    </div>
+
+                    <div className="row mt-2 text-center">
+                      <h5>Se parte de nuestra comunidad</h5>
+                      <h5>
+                        Descubre increibles tips de bienestar y compra los
+                      </h5>
+                      <h5>mejores productos de tu ciudad.</h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div className="container col-12">
+                      <div className="row text-center mb-2">
+                        <p>
+                          <font color="red">
+                            <i>
+                              <u> Suscribete a nuestros boletines mensuales</u>
+                            </i>
+                          </font>
+                        </p>
+                        <div className="col-4">
+                          <input
+                            type="email"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Ingresa tu Correo"
+                          />
+                        </div>
+
+                        <div className="col-4">
+                          <select
+                            class="form-select"
+                            aria-label="Default select example"
+                          >
+                            <option selected>Añadir una Ciudad</option>
+                            <option value="1">Medellin</option>
+                            <option value="2">Bogota</option>
+                            <option value="3">Cali</option>
+                            <option value="3">Pereira</option>
+                            <option value="3">Manizales</option>
+                            <option value="3">Cucuta</option>
+                          </select>
+                        </div>
+                        <div className="col-4">
+                          <button
+                            className="btn btn-outline-success"
+                            data-bs-dismiss="modal"
+                          >
+                            Guardar
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
                 </div>
 
-                <div className="row mt-2 text-center">
-                  <h5>Se parte de nuestra comunidad</h5>
-                  <h5>Descubre increibles tips de bienestar y compra los</h5>
-                  <h5>mejores productos de tu ciudad.</h5>
+                <div class="modal-footer ">
+                  <form>
+                    <div className="container d-flex text-center">
+                      <div className="row">
+                        <p>
+                          Puedes cambiar tus preferencias de suscripción en
+                          cualquier momento
+                        </p>
+                        <p>
+                          Al hacer click en Guardar, aceptas los Términos de
+                          servicio y la Política de Privacidad
+                        </p>
+                        <a href="/usuarioPublico">
+                          ¿Ya tienes cuenta? Ingresar
+                        </a>
+                        <a href="#" data-bs-dismiss="modal">
+                          No gracias
+                        </a>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
-            <div class="modal-body">
-              <form>
-                <div className="container col-12">
-                  <div className="row text-center mb-2">
-                    <p>
-                      <font color="red">
-                        <i>
-                          <u> Suscribete a nuestros boletines mensuales</u>
-                        </i>
-                      </font>
-                    </p>
-                    <div className="col-4">
-                      <input
-                        type="email"
-                        class="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"
-                        placeholder="Ingresa tu Correo"
-                      />
-                    </div>
-
-                    <div className="col-4">
-                      <select
-                        class="form-select"
-                        aria-label="Default select example"
-                      >
-                        <option selected>Añadir una Ciudad</option>
-                        <option value="1">Medellin</option>
-                        <option value="2">Bogota</option>
-                        <option value="3">Cali</option>
-                        <option value="3">Pereira</option>
-                        <option value="3">Manizales</option>
-                        <option value="3">Cucuta</option>
-                      </select>
-                    </div>
-                    <div className="col-4">
-                      <button
-                        className="btn btn-outline-success"
-                        data-bs-dismiss="modal"
-                      >
-                        Guardar
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-
-            <div class="modal-footer ">
-              <form>
-                <div className="container d-flex text-center">
-                  <div className="row">
-                    <p>
-                      Puedes cambiar tus preferencias de suscripción en
-                      cualquier momento
-                    </p>
-                    <p>
-                      Al hacer click en Guardar, aceptas los Términos de
-                      servicio y la Política de Privacidad
-                    </p>
-                    <a href="/usuarioPublico">¿Ya tienes cuenta? Ingresar</a>
-                    <a href="#" data-bs-dismiss="modal">
-                      No gracias
-                    </a>
-                  </div>
-                </div>
-              </form>
-            </div>
           </div>
-        </div>
-      </div>
-     /* fin modal */
-
+        )
+        /* fin modal */
       }
-
-      
 
       <div className="container p-5 " id="inicio1">
         <div className="container p-5 mt-3"></div>
@@ -185,14 +177,13 @@ export default function inicio() {
               <h3 className="mt-4">
                 <strong>
                   {" "}
-                  Ser reconocidos como empresa líder en Colombia distinguiéndonos
-                  por la calidad y eficacia de nuestros productos dirigidos a la
-                  salud y el bienestar, reconocidos mundialmente por la
-                  capacidad de generar bienestar, brindando conocimiento y
-                  herramientas para mejorar la calidad de vida.
+                  Ser reconocidos como empresa líder en Colombia
+                  distinguiéndonos por la calidad y eficacia de nuestros
+                  productos dirigidos a la salud y el bienestar, reconocidos
+                  mundialmente por la capacidad de generar bienestar, brindando
+                  conocimiento y herramientas para mejorar la calidad de vida.
                 </strong>
               </h3>
-             
             </div>
             <div className="col-sm-12 col-md-12 text-center">
               <h1 className="mt-4">
@@ -211,7 +202,6 @@ export default function inicio() {
                   espiritual. Llevando salud y bienestar a todas las familias.
                 </strong>
               </h3>
-              
             </div>
           </div>
         </div>
@@ -221,48 +211,91 @@ export default function inicio() {
         <div className="container">
           <h1 className="text-center mt-4 "> NUESTROS VALORES</h1>
           <div className="row mt-5">
-            <div className="col-sm-12 col-md-3 text-center">
-              <div className="">
+            <div className="col-md-12 col-lg-3 text-center">
+              <div className="marco1">
+                <div className="tablaigual">
                 <div className="card-boddy">
-                  <h4><strong>PASION</strong></h4>
-                  <p className="mt-2">
-                  <font font  size = '4'><strong>Por medio de nustros canales de difucion, pretendemos llegar
-                    a todos los lugares de colombia yde LatinoAmerica.</strong></font>
+                  <h4>
+                    <strong>PASION</strong>
+                  </h4>
+                  <p min className="mt-2">
+                    <font font size="4">
+                      <strong>
+                        ¡Estamos cambiando el mundo saludable! y para seguirlo
+                        haciendo el compromiso es ilimitado. a todos los lugares
+                        de colombia y de LatinoAmerica.
+                      </strong>
+                    </font>
                   </p>
                 </div>
+                </div>
+                
+              </div>
+              
+            </div>
+            <div className="col-md-12 col-lg-3 text-center">
+              <div className="marco1">
+                <div className="tablaigual">
+                <div className="card-boddy">
+                  <h4>
+                    <strong>TRANSPARENCIA</strong>
+                  </h4>
+                  <p min className="mt-2">
+                    <font font size="4">
+                      <strong>
+                      Somos auténticos y sinceros, no nos interesa cambiar la verdad para sacar provecho.
+                      </strong>
+                    </font>
+                  </p>
+                </div>
+                </div>
+                
+              </div>
+              
+            </div>
+            <div className="col-md-12 col-lg-3 text-center">
+              <div className="marco1">
+                <div className="tablaigual">
+                <div className="card-boddy">
+                  <h4>
+                    <strong>EMPATIA</strong>
+                  </h4>
+                  <p min className="mt-2">
+                    <font font size="4">
+                      {" "}
+                      <strong>
+                        Conectamos con nuestro cliente, poniéndonos en su lugar
+                        para comprender sus necesidades y lograr prestarle
+                        nuestra ayuda.
+                      </strong>
+                    </font>
+                  </p>
+                </div>
+                </div>
+                
               </div>
             </div>
-            <div className="col-sm-12 col-md-3 text-center">
-              <div className="">
+            <div className="col-md-12 col-lg-3 text-center">
+              <div className="marco1">
+                <div className="tablaigual">
                 <div className="card-boddy">
-                  <h4><strong>TRANSPARENCIA</strong></h4>
-                  <p className="mt-2">
-                  <font font  size = '4'> <strong> Por medio de nustros canales de difucion, pretendemos llegar
-                    a todos los lugares de colombia yde LatinoAmerica.</strong></font>
+                  <h4>
+                    <strong>CALIDAD</strong>
+                  </h4>
+                  <p min className="mt-2">
+                    <font font size="4">
+                      {" "}
+                      <strong>
+                        {" "}
+                        Nos comprometemos, que los procesos de nuestros
+                        productos sean los mejores posibles, no nos conformamos
+                        con menos.
+                      </strong>
+                    </font>
                   </p>
                 </div>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-3 text-center">
-              <div className="">
-                <div className="card-boddy">
-                  <h4><strong>EMPATIA</strong></h4>
-                  <p className="mt-2">
-                  <font font  size = '4'> <strong> Por medio de nustros canales de difucion, pretendemos llegar
-                    a todos los lugares de colombia yde LatinoAmerica.</strong></font>
-                  </p>
                 </div>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-3 text-center">
-              <div className="">
-                <div className="card-boddy">
-                  <h4><strong>CALIDAD</strong></h4>
-                  <p className="mt-2">
-                  <font font  size = '4'> <strong> Por medio de nustros canales de difucion, pretendemos llegar
-                    a todos los lugares de colombia yde LatinoAmerica.</strong></font>
-                  </p>
-                </div>
+                
               </div>
             </div>
           </div>
