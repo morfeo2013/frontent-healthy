d66937cd8c1d16e59d99b93e336a38b38c9c347d
unknown <jhonrios@hotmail.es>
2021-06-21 23:08:04 -0500

    mejora visual pagina carrito tamaño mejora

--- src/components/carrito.js
+++ src/components/carrito.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function carrito() {
     return (
-        <div className="container p-5 text-center" >
+        <div className="container p-5 col-xs-12 col-lg-8 text-center" >
             <div className="container p-5 mt-3">
-            <img className="mt-5" src="https://res.cloudinary.com/dhiasghho/image/upload/v1617378225/zb7snsoxqk0rt3mr0arv.jpg" alt=""/>
+            <img className="d-block w-100 mt-5" src="https://res.cloudinary.com/dhiasghho/image/upload/v1617378225/zb7snsoxqk0rt3mr0arv.jpg" alt=""/>
             </div>
            
         </div>
