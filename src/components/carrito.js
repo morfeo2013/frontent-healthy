732acc0fd0e8f47c6349749c0a1fb375f333c591
unknown <jhonrios@hotmail.es>
2021-06-21 20:59:58 -0500

    mejora visual carrito1

---
+++ src/components/carrito.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function carrito() {
+    return (
+        <div className="container p-5 text-center" >
+            <div className="container p-5 mt-3">
+            <img className="mt-5" src="https://res.cloudinary.com/dhiasghho/image/upload/v1617378225/zb7snsoxqk0rt3mr0arv.jpg" alt=""/>
+            </div>
+           
+        </div>
+    )
+}
+
+export default carrito
